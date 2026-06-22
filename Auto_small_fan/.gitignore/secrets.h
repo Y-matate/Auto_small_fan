bbc3f6dae64e9a94fef88fe0709d59213b7a19c1
Y_matate <matatey@outlook.jp>
2026-06-22 15:11:33 +0900

    修正

--- Auto_small_fan/.gitignore/secrets.h
+++ Auto_small_fan/.gitignore/secrets.h
@@ -8,10 +8,10 @@
 // ===== SwitchBot（ライトを挿したプラグ）=====
 #define SB_TOKEN    "自身で取得したswitchbotのトークン"
 #define SB_SECRET   "自身で取得したswitchbotのシークレット"
-#define SB_DEVICE_STR "AC276E40DF66"
+#define SB_DEVICE_STR "自身で取得したデバイスID"
 
 // ===== Discord Webhook =====
 // "/api/webhooks/～" 以降だけを設定してください
-#define DISCORD_PATH_STR "/api/webhooks/1516311906967621722/AXdS3856AUZvprju1yKrvt7X3ZXhIrAXB0m6rRGjcwj1v8z3ka5v1NUw2IUZ9ronrs1P"
+#define DISCORD_PATH_STR "/api/webhooks/自身が作成したdiscordサーバーのURL"
 
 #endif // SECRETS_H
